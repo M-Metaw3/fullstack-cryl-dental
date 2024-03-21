054056171dd1796ca39665ca000dfd6a50e3cc19
M-Metaw3 <mizoomizoo161@gmail.com>
2024-03-21 09:02:48 +0200

    edits

--- index.js
+++ index.js
@@ -109,8 +109,9 @@ const Home = require('./src/routes/HomeRoute');
 require('./dbconfig');
 
 const app = express();
-
-app.use(express.json({ limit: '1mb' }));
+app.use(bodyParser.json({ limit: '50mb' })); // Increase the limit as needed
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
 
 app.use(cookieParser());
 app.use(morgan('dev'));
